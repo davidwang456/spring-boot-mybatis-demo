a0bdb3c568f52b563dcef677426640cef517aca5
davidwang456 <davidwang456@sina.com>
2017-04-21 17:34:12 +0800

    tomcat vs jetty vs undertow

---
+++ SpringBootContainersCompare/src/main/java/com/davidwang456/examples/boot/app/Application.java
@@ -0,0 +1,18 @@
+package com.davidwang456.examples.boot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Controller
+@EnableWebMvc
+@SpringBootApplication
+public class Application extends WebMvcConfigurerAdapter{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ SpringBootContainersCompare/src/main/java/com/davidwang456/examples/boot/app/model/Teacher.java
@@ -0,0 +1,23 @@
+package com.davidwang456.examples.boot.app.model;
+
+public class Teacher extends Person {
+	private int salary;
+	private int yearsOfExperience;
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+
+}
